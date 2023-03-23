91bc4ecbfbb1e2c2fc268e556f4521b3abc01431
kath3rine <not.katherine.li@gmail.com>
2023-03-23 01:02:17 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Test
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
