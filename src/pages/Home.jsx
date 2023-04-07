b893b426bfdc6574f3ad940718776176915b3770
kath3rine <not.katherine.li@gmail.com>
2023-04-07 03:07:15 -0400

    4/7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../styles/styles.css';
 import YoutubeEmbed from '../components/YoutubeEmbed';
 import Mandelbrot from '../images/mandelbrot.jpg';
@@ -10,7 +11,8 @@ function Home() {
             <div className="homebody">
                 <h1>FRACTAL EXPLORER</h1>
                 <p> 
-                Brief about the project (note: the current pictures/videos are just placeholders for now and will be replaced with actual documentation of the project)
+                <Link to="/about">Fractal Explorer</Link> utilizes technology to combine elements of art, music, math, and psychology to create a moving virtual reality experience. The user chooses a song, and are placed within an immersive animated, recursively generated three-dimensional fractal that changes color, shape, etc. to complement the song. 
+                <br /><br />(note: the current pictures/videos are just placeholders for now and will be replaced with actual documentation of the project)
                 </p>
                 <div id="video">
                     <YoutubeEmbed embedId="dQw4w9WgXcQ" />
